2f102d6a27d5c21eba84ec436a2e0c69b40864f6
chiyuan <chiyuan.goh@gmail.com>
2018-05-24 18:18:11 +0800

    best performance so far.
    change back to deconv instead of upsampling + conv as doesnt seem to make much difference. rather it is slower
    due to padding/cropping.
    
    adjust class weights to others=.25,road=2,car=5
    use a mask to set hood pixels to 0 before training.
    
    previous commit regarding big jump is false, as duplicate code on notebook uses back deconv with no class weights which is
    same as before. I have no idea why there is a jump in test performace.

--- experiment.py
+++ experiment.py
@@ -4,8 +4,9 @@ import os
 import matplotlib.pyplot as plt
 
 from keras.models import Sequential, load_model
-from keras.layers.convolutional import Conv2D, Deconvolution2D, UpSampling2D
+from keras.layers.convolutional import Conv2D, Deconvolution2D, UpSampling2D, ZeroPadding2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
+from keras.layers.normalization import BatchNormalization
 from keras.layers import Activation
 from keras.activations import softmax
 from keras.layers.core import Reshape
@@ -13,19 +14,23 @@ from keras.layers.core import Reshape
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
-burl = '/ext/data/lyft-challenge/Train'
+#burl = '/ext/data/lyft-challenge/Train'
+burl = '/home/cy/Desktop/Train'
 img_url = os.path.join(burl, "CameraRGB")
 label_url = os.path.join(burl, "CameraSeg")
+
+test_img_gl = os.listdir(label_url)[0]
+label_gl = cv2.imread(os.path.join(label_url, test_img_gl) )[:,:,2]
+veh_pixels_gl = (label_gl == 10).nonzero()
+hood_indices_gl = (veh_pixels_gl[0] >= 496).nonzero()[0]
+hood_pixels_gl = (veh_pixels_gl[0][hood_indices_gl], \
+               veh_pixels_gl[1][hood_indices_gl])
 
 def process_label(yimg):
     #replace lane markings 
     label_ch = yimg[:,:,2]
-    veh_pixels = (label_ch == 10).nonzero()
-    hood_indices = (veh_pixels[0] >= 496).nonzero()[0]
-    hood_pixels = (veh_pixels[0][hood_indices], \
-                   veh_pixels[1][hood_indices])
-    
-    label_ch[hood_pixels] = 0
+
+    label_ch[hood_pixels_gl] = 0
     label_ch[label_ch == 6] = 7
     
     h, w = label_ch.shape
@@ -35,13 +40,19 @@ def process_label(yimg):
     cls_img[:,:, 0] = label_ch == 10
     cls_img[:,:, 1] = label_ch == 7
     cls_img[:,:, 2] = (label_ch != 10) & (label_ch != 7)
+
+    weights = np.zeros_like(label_ch, dtype='float')
+    weights[label_ch == 10] = 5.
+    weights[label_ch == 7] = 2.
+    weights[(label_ch != 10) & (label_ch != 7)] = .25
         
-    return cls_img
+    return cls_img, weights
 
 def generate_samples(xpaths, ypaths, batch_size, train=False):
     batch_size = batch_size
 
     #cars, road, none
+
     cweights = [14.259816, 1., 0.25325792]
 
     while 1:
@@ -59,47 +70,32 @@ def generate_samples(xpaths, ypaths, batch_size, train=False):
             for n in range(len(xs)):
                 ximg = cv2.imread(os.path.join(img_url, xs[n]))
                 yimg = cv2.imread((os.path.join(label_url, ys[n])))
+
+                ximg[hood_pixels_gl[0],hood_pixels_gl[1], :] = 0.
+                hot_label, weight = process_label(yimg)
                 
-                ydata.append(process_label(yimg))
+                ydata.append(hot_label)
                 xdata.append(ximg)
+                sample_weights.append(weight)
 
-                if train:
-                    sample_weight = np.zeros( (ximg.shape[0], ximg.shape[1]) )
-                    car_indices = np.where(ydata[-1][:, :, 0] == 1)
-                    road_indices = np.where(ydata[-1][:, :, 1] == 1)
-                    others_indices = np.where(ydata[-1][:,:,2] == 1)
-                    sample_weight[car_indices] = 5.
-                    sample_weight[road_indices] = 1.
-                    sample_weight[others_indices] = .25
-                    sample_weight = sample_weight.reshape(600*800)
-                    sample_weights.append(sample_weight)
-
-
-            #reflect 1
-            ridx = np.random.choice(range(batch_size))
-            reflect_img = cv2.flip(xdata[ridx], 1)
-            reflect_label = cv2.flip(ydata[ridx], 1)
-            xdata.append(reflect_img)
-            ydata.append(reflect_label)
-            
-            sample_weight = np.zeros( (reflect_img.shape[0], reflect_img.shape[1]) )
-            car_indices = np.where(ydata[-1][:, :, 0] == 1)
-            road_indices = np.where(ydata[-1][:, :, 1] == 1)
-            others_indices = np.where(ydata[-1][:,:,2] == 1)
-            sample_weight[car_indices] = 5.
-            sample_weight[road_indices] = 1.
-            sample_weight[others_indices] = .25
-            sample_weight = sample_weight.reshape(600*800)
-            sample_weights.append(sample_weight)
+                #reflect 1
+                ridx = np.random.choice(range(batch_size))
+                reflect_img = cv2.flip(ximg, 1)
+                reflect_label = cv2.flip(hot_label, 1)
+                reflect_weight = cv2.flip(weight, 1)
+                xdata.append(reflect_img)
+                ydata.append(reflect_label)
+                sample_weights.append(reflect_weight)
 
             xdata = np.array(xdata).astype("float")
-            xdata = xdata/255. - 0.5
-            ydata = np.array(ydata).reshape( (batch_size+1, 600*800, 3))
-            
+            xdata = xdata/255.# - 0.5
+            #ydata = np.array(ydata).reshape( (batch_size, 600*800, 3))
+            ydata = np.array(ydata).reshape( (batch_size * 2, 600*800, 3))
+            sample_weights = np.array(sample_weights).reshape( (batch_size * 2, 800*600))
+
             if not train:
                 yield xdata, ydata
             else:
-                #print(ydata.shape[:sample_weight.ndim], np.array(sample_weights).shape )
                 yield xdata, ydata, np.array(sample_weights)
 
 
@@ -110,59 +106,84 @@ def segnet(num_classes, img_shape):
     #cropping and pre-process
     #model.add()
     #model.add()
-    
+
+    #model.add(ZeroPadding2D((4, 0), input_shape=img_shape))
+
     #encoders
-    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1), activation='relu', border_mode='same', input_shape=img_shape))
+    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1), border_mode='same', input_shape=img_shape))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), border_mode='same'))
     
-    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1), activation='relu', border_mode='same'))
+    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1), border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), border_mode='same'))
     
-    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1), activation='relu', border_mode='same'))
+    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1),  border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), border_mode='same'))
     
-    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1), activation='relu', border_mode='same'))
+    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1),  border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), border_mode='same'))
 
     #decoders
-    model.add(UpSampling2D(2,2))
-    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1), activation='relu', border='same'))
-
-    model.add(UpSampling2D(2, 2))
-    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1, 1), activation='relu', border='same'))
-
-    model.add(UpSampling2D(2, 2))
-    model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1, 1), activation='relu', border='same'))
-
-    model.add(UpSampling2D(2, 2))
-    model.add(Conv2D(num_classes, nb_row=7, nb_col=7, subsample=(1, 1), activation='relu', border='same'))
+    # model.add(UpSampling2D(size=(2,2)))
+    # model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1,1), border_mode='same'))
+    # model.add(BatchNormalization())
+    # model.add(Activation('relu'))
+    #
+    # model.add(UpSampling2D(size=(2, 2)))
+    # model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1, 1), border_mode='same'))
+    # model.add(BatchNormalization())
+    # model.add(Activation('relu'))
+    #
+    #
+    # model.add(UpSampling2D(size=(2, 2)))
+    # model.add(Conv2D(num_features, nb_row=7, nb_col=7, subsample=(1, 1), border_mode='same'))
+    # model.add(BatchNormalization())
+    # model.add(Activation('relu'))
+    #
+    # model.add(UpSampling2D(size=(2, 2)))
+    # model.add(Conv2D(num_classes, nb_row=7, nb_col=7, subsample=(1, 1), border_mode='same'))
+    # model.add(BatchNormalization())
+    # model.add(Activation('relu'))
+    #
+    #
+    # model.add(Cropping2D(cropping=((4,4), (0,0))) )
 
     #decoders
-    # model.add(Deconvolution2D(num_features, nb_row=8, nb_col=8,
-    #           subsample=(2,2),
-    #           border_mode='same',
-    #           output_shape=(None, img_shape[0]//(2**3), img_shape[1]//(2**3), num_features),
-    #           activation='relu'))
-    #
-    # model.add(Deconvolution2D(num_features, nb_row=8, nb_col=8,
-    #       subsample=(2,2),
-    #       border_mode='same',
-    #       output_shape=(None, img_shape[0]//(2**2), img_shape[1]//(2**2), num_features),
-    #       activation='relu'))
-    #
-    # model.add(Deconvolution2D(num_features, nb_row=8, nb_col=8,
-    #       subsample=(2,2),
-    #       border_mode='same',
-    #       output_shape=(None, img_shape[0]//(2**1), img_shape[1]//(2**1), num_features),
-    #       activation='relu'))
-    #
-    # model.add(Deconvolution2D(num_classes, nb_row=8, nb_col=8,
-    #       subsample=(2,2),
-    #       border_mode='same',
-    #       output_shape=(None, img_shape[0], img_shape[1], num_classes),
-    #       activation='relu'))
+    model.add(Deconvolution2D(num_features, nb_row=8, nb_col=8,
+              subsample=(2,2),
+              border_mode='same',
+              output_shape=(None, img_shape[0]//(2**3), img_shape[1]//(2**3), num_features)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(Deconvolution2D(num_features, nb_row=8, nb_col=8,
+          subsample=(2,2),
+          border_mode='same',
+          output_shape=(None, img_shape[0]//(2**2), img_shape[1]//(2**2), num_features)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(Deconvolution2D(num_features, nb_row=8, nb_col=8,
+          subsample=(2,2),
+          border_mode='same',
+          output_shape=(None, img_shape[0]//(2**1), img_shape[1]//(2**1), num_features)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(Deconvolution2D(num_classes, nb_row=8, nb_col=8,
+          subsample=(2,2),
+          border_mode='same',
+          output_shape=(None, img_shape[0], img_shape[1], num_classes)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     
-    print("nclass", num_classes)
     model.add(Reshape((800*600, num_classes)))
 #     prediction layer
     model.add(Activation("softmax"))
@@ -171,30 +192,30 @@ def segnet(num_classes, img_shape):
     
     return model
 
-def run():
-    batch_size=4
-    #img_size = 
-    
-    xdata =  os.listdir(img_url)
-    ylabel = os.listdir(label_url)
-    
-    xtrain, xtest, ytrain, ytest = train_test_split(xdata, ylabel, test_size=0.2)
-    train_gen = generate_samples(xtrain, ytrain, batch_size=batch_size, train=True)
-    test_gen = generate_samples(xtest, ytest, batch_size=batch_size)
-    
-    imgsize = (600, 800, 3)
-    model = segnet(3, imgsize)
-    #print(len(xtrain))
-    runobj = model.fit_generator(train_gen,
-                                samples_per_epoch= len(xtrain)//batch_size * batch_size * 1.25,
-                                nb_epoch=7,
-                                validation_data=test_gen, 
-                                nb_val_samples = (len(xtest)//batch_size) * batch_size,
-                                nb_worker=2,
-                                nb_val_worker=2,
-                                verbose=1)
-        
-    model.save("m2")
+# def run():
+#     batch_size=4
+#     #img_size =
+#
+#     xdata =  os.listdir(img_url)
+#     ylabel = os.listdir(label_url)
+#
+#     xtrain, xtest, ytrain, ytest = train_test_split(xdata, ylabel, test_size=0.2)
+#     train_gen = generate_samples(xtrain, ytrain, batch_size=batch_size, train=True)
+#     test_gen = generate_samples(xtest, ytest, batch_size=batch_size)
+#
+#     imgsize = (600, 800, 3)
+#     model = segnet(3, imgsize)
+#     #print(len(xtrain))
+#     runobj = model.fit_generator(train_gen,
+#                                 samples_per_epoch= len(xtrain)//batch_size * batch_size * 1.25,
+#                                 nb_epoch=7,
+#                                 validation_data=test_gen,
+#                                 nb_val_samples = (len(xtest)//batch_size) * batch_size,
+#                                 nb_worker=2,
+#                                 nb_val_worker=2,
+#                                 verbose=1)
+#
+#     model.save("m2")
     
 if __name__ == '__main__':
     run()
